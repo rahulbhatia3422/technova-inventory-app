ba36d72bcc32d1582882ffc0bd317e71be9ee0b1
Rahul Bhatia <112072457+rahulbhatia3422@users.noreply.github.com>
2025-05-13 13:12:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,4 +7,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')  # Ensure accessible from Docker
